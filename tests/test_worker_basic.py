705dbc2b7cf5d8cddd41e46b30b0cd71c5bfbf0c
Whitney Dluhosh <whitneydluhosh@hotmail.com>
2017-07-25 22:29:57 -0700

    dooo it

--- tests/test_worker_basic.py
+++ tests/test_worker_basic.py
@@ -91,3 +91,5 @@ class TestWorkerBasic(unittest.TestCase):
         results, next_page = worker.parse_text(str(text).strip().replace('\r\n', ''))
         temp = ''.join(item[0] for item in results)
         self.assertGreater(temp[0].length,0)
+        self.assertGreater(temp[1].length,0)		
+        self.assertGreater(temp[2].length,0) 
